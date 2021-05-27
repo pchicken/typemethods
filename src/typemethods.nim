563bb75aabef002d3c1be764fffc9ad18232aad7
chicken <40350296+pchicken@users.noreply.github.com>
2021-05-27 11:14:11 -0400

    Create typemethods.nim

---
+++ src/typemethods.nim
@@ -0,0 +1,14 @@
+import macros
+
+macro typemethod*(t: typedesc, fn: untyped): untyped =
+    fn.expectKind nnkProcDef
+    result = fn
+    let typeParaem = nnkIdentDefs.newTree(
+        ident("TYPEMETHOD_TYPE"),
+        nnkBracketExpr.newTree(
+            ident("typedesc"),
+            t,
+        ),
+        newEmptyNode(),
+    )
+    result[3].insert(1, typeParam)
